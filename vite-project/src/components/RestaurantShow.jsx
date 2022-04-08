4fcea73961864421437ada0655aed679a1676e1d
Suraj Pathak <pathaksuraj136@gmail.com>
2022-04-08 10:56:50 -0700

    added som features

--- vite-project/src/components/RestaurantShow.jsx
+++ vite-project/src/components/RestaurantShow.jsx
@@ -4,19 +4,19 @@ import style from "../components/res.css"
 
 export const RestaurantShow = ()=>{
 const [data,setdata] = useState([])
-const [count,setcount] = useState(5)
+const [count,setcount] = useState(3)
+const [payments,setpayments] = useState("cash")
 React.useEffect(()=>{
     getData()
     
-},[count])
+},[count,payments])
     const getData = ()=>{
     axios.get("http://localhost:8080/getres",{
         params:{
             _page : 1,
             _limit: 5,
             ratings:count,
-            // payment_methods:"card"
-            
+            payment_methods:payments
         }
     }).then(function(res){
        setdata(res.data)
@@ -26,43 +26,31 @@ React.useEffect(()=>{
 
     const handelSortsecond = (value)=>{
          setcount(value)
-        //  count = 0
          console.log(count)
     }
-
-
-    
-
-    // React.useEffect(() => {
-    //     axios.get("http://localhost:8080/getres", {
-    //       params:{
-    //         _page : 1,
-    //         _limit: 5,
-    //         ratings:Ratings,
-    //         Payment:"card"
-    //       }
-    //     }).then(res => setData(res.data));
-    //        // console.log("new",data);
-    //        console.log("rate",ratings)
-    //    }, [])
-       
 
     const handelSort = ()=>{
         const sortdata = [...data]
        sortdata.sort(function(a,b){
-           return a.ratings - b.ratings
+           return a.costForTwo - b.costForTwo
        })
        setdata(sortdata)
     }
     const handelSor2 = ()=>{
         const sortdata = [...data]
         sortdata.sort((a,b)=>(
-       b.ratings - a.ratings
+       b.costForTwo - a.costForTwo
     )
 )
  setdata(sortdata)
 console.log("sort",data)
     }
+
+const getall = ()=>{
+    axios.get("http://localhost:8080/getres").then(function(res){
+       setdata(res.data)
+    })
+}
 
     return(
         <div className="main">
@@ -71,9 +59,9 @@ console.log("sort",data)
 <button onClick={()=>handelSortsecond(3)}>3 Star</button>
 <button onClick={()=>handelSortsecond(4)}>4 Star</button>
 <button onClick={()=>handelSortsecond(5)}>5 Star</button>
-{/* <button onClick={()=>handelCard()}>Card</button> */}
-{/* <button>Cash</button>
-<button>All</button>  */}
+<button onClick={()=>setpayments("card")}>Card</button>
+<button onClick={()=>setpayments("cash")}>Cash</button>
+<button onClick={()=> getall()}>All</button>
 <button onClick={()=>handelSort()}>Asc</button>
 <button onClick={()=>handelSor2()}>Dsc</button>
 
@@ -84,6 +72,7 @@ console.log("sort",data)
   <div>
   <h3>Name - {el.name}</h3>
  <p>Cost for two - {el.costForTwo}</p>
+ <p>Accepts - {el.payment_methods}</p>
  <p>Ratings - {el.ratings}</p>
  <p>Votes - {el.votes}</p>
  <p>Reviews - {el.reviews}</p>
